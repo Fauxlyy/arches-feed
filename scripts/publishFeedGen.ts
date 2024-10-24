0917eb542a24b58efd2864a52afa13b48dfe1800
Fauxlyy <cathensfaux@gmail.com>
2024-10-24 15:54:03 -0400

    Published

--- scripts/publishFeedGen.ts
+++ scripts/publishFeedGen.ts
@@ -25,7 +25,7 @@ const run = async () => {
 
   // (Optional) A description of your feed
   // Ex: Top trending content from the whole network
-  const description = 'A feed for fans of the visual novel Arches including posts with #archesvn or that include enough Arches related words (Cameron, Devon, Arches, vn...)'
+  const description = 'A feed for fans of the visual novel Arches\n\nIncludes posts that have the hashtag #archesvn or that contain enough Arches related words (Cameron, Devon, Arches, vn...)'
 
   // (Optional) The path to an image to be used as your feed's avatar
   // Ex: ~/path/to/avatar.jpeg
